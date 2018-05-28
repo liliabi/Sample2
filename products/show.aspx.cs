4601419a3b9ff13112baf747eed920a59ae5010c
liliabi <78186700@sina.com>
2018-05-28 14:53:28 +0800

    Create show.aspx.cs

---
+++ products/show.aspx.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ayzhuangxiu.products
+{
+    public partial class show : System.Web.UI.Page
+    {
+        public string bContent;
+        public string bTitle;
+        public string bPic;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.ltHeader.Text = ayzhuangxiu.common.NavClass.LoadHeader();
+                this.ltFooter.Text = ayzhuangxiu.common.NavClass.LoadFooter();
+                BindDataList();
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "onekey", "alert('" + ex.Message + "')", true);
+            }
+        }
+
+        private void BindDataList()
+        {
+            string id = string.Empty;
+            if (Request.QueryString.Count > 0)
+            {
+                id = PaducnSoft.Common.Utils.NullToString(Request.QueryString["id"]);
+            }
+
+            string sql = "select a.* from ay_products_v a where 1=1  ";
+            if (id != "")
+            {
+                sql += " and (a.bId=" + id + ")";
+            }
+            //sql += " order by a.bAddTime desc,a.bId";
+            DataTable dt = PaducnSoft.DBUtility.DbHelperOleDb.Query(sql).Tables[0];
+            bContent = Server.HtmlDecode(dt.Rows[0]["bContent"].ToString());
+            bTitle = dt.Rows[0]["bTitle"].ToString();
+            bPic = dt.Rows[0]["bPic"].ToString();
+        }
+    }
+}
